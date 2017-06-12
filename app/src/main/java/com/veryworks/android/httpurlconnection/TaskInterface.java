5bc0415dbbaaaad4a9a65c9c9df16ea924c7a259
javafa <nigne2016@gmail.com>
2017-06-12 16:57:30 +0900

    add interface

---
+++ app/src/main/java/com/veryworks/android/httpurlconnection/TaskInterface.java
@@ -0,0 +1,10 @@
+package com.veryworks.android.httpurlconnection;
+
+/**
+ * Created by pc on 6/12/2017.
+ */
+
+public interface TaskInterface {
+    public String getUrl();
+    public void postExecute(String result);
+}
